60f6e762f1d528d793896cfc108a7c3aa7cf336e
Borys Ushakov <inlvert@gmail.com>
2024-04-08 12:28:51 +0300

    added model User + router and controller

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema ({
+  firstName: {
+    type: String,
+    required: [true, 'First name is required']
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isOnline: {
+    type: Boolean,
+    default: false
+  }
+  // messages: [{
+  //   tepe: Schema.Types.ObjectId,
+  //   ref: 'Message'
+  // }]
+}, {
+  timestamps: true
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
---
+++ routers/userRouter.js
@@ -0,0 +1,9 @@
+const userRouter = require('express').Router();
+const userController = require('../controllers/user.controller');
+
+userRouter.post('/', userController.createUser);
+
+
+
+
+module.exports = userRouter;
