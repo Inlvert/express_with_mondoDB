02cb2c5390d08610490f3ac7b62b6fcd98043659
Borys Ushakov <inlvert@gmail.com>
2024-04-09 11:58:35 +0300

    add

--- controllers/messege.controller.js
+++ controllers/messege.controller.js
@@ -1,70 +1,104 @@
-const createHttpError = require('http-errors');
-const { Message } = require('../models');
+const createHttpError = require("http-errors");
+const { Message } = require("../models");
 
 module.exports.createMessage = async (req, res, next) => {
   try {
-    const {user, body} = req;
+    const { user, body } = req;
 
     const message = await Message.create({
       ...body,
-      user: user._id
-    })
+      user: user._id,
+    });
 
-    console.log(user)
+    console.log(user);
 
-    res.status(201).send({data: message});
-
+    res.status(201).send({ data: message });
   } catch (error) {
     next(error);
   }
-}
+};
 
 module.exports.getAllMessages = async (req, res, next) => {
   try {
-
     const messages = await Message.find();
 
-    res.send({data: messages});
-
-  } catch(error) {
+    res.send({ data: messages });
+  } catch (error) {
     next(error);
   }
-}
+};
 
 module.exports.getMessage = async (req, res, next) => {
   try {
-    const {params: {messageId}, user } = req;
+    const {
+      params: { messageId },
+      user: { _id: userId },
+    } = req;
 
     const message = await Message.findOne({
       _id: messageId,
-      user
-    })
+      user: userId,
+    });
 
-    console.log(req);
-
-    if(!message) {
-      return (next(createHttpError(404, 'message not found')))
+    if (!message) {
+      return next(createHttpError(404, "Message not found"));
     }
 
-    res.send({data: message});
-  } catch (error) {
-    next(error)
-  }
-}
-
-module.exports.updateMessage = async (req, res, next) => {
-  try {
-    const {params: {messageId}, user, body } = req;
-
-    const updatedMessage = await Message.findOneAndUpdate({
-      _id: messageId,
-      user: user
-    }, body, {new: true});
-
-    res.send({data: updatedMessage})
-
-
+    res.send({ data: message });
   } catch (error) {
     next(error);
   }
-}
+};
+
+module.exports.updateMessage = async (req, res, next) => {
+  try {
+    const {
+      user: { _id: userId },
+      params: { messageId },
+      body,
+    } = req;
+
+    const updatedMessage = await Message.findOneAndUpdate(
+      {
+        _id: messageId,
+        user: userId,
+      },
+      body,
+      { new: true }
+    );
+
+    if (!updatedMessage) {
+      return next(createHttpError(404, "Message not found"));
+    }
+
+    console.log(messageId);
+    console.log(userId);
+    console.log(updatedMessage);
+
+    res.send({ data: updatedMessage });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteMessage = async (req, res, next) => {
+  try {
+    const {
+      user: { _id: userId },
+      params: { messageId },
+    } = req;
+
+    const message = await Message.findOneAndDelete({
+      _id: messageId,
+      user: userId,
+    });
+
+    if (!message) {
+      return next(createHttpError(404, "message not found"));
+    }
+
+    res.send({ data: message });
+  } catch (error) {
+    next(error);
+  }
+};
--- routers/messageRouter.js
+++ routers/messageRouter.js
@@ -5,6 +5,7 @@ messageRouter.post('/', messageController.createMessage)
 messageRouter.get('/', messageController.getAllMessages)
 messageRouter.get('/:messageId', messageController.getMessage)
 messageRouter.put('/:messageId', messageController.updateMessage)
+messageRouter.delete('/:messageId', messageController.deleteMessage)
 
 
 module.exports = messageRouter;
